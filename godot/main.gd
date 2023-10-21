5293642d57ff6f7b1303840d85ba1ba8ebde418c
David Goodwin <david@goodwinz.net>
2023-10-21 13:39:48 -0700

    Have camera circle the arena

--- godot/main.gd
+++ godot/main.gd
@@ -4,19 +4,11 @@ extends Node
 @onready var camera := $CameraPivot
 # Meters per second that the camera moves
 var camera_move_speed : int = 20
-# Mouse sensitivity for camera turning
-var camera_turn_sensitivity : float = 0.01
-# The x, y, z rotations of the camera. These are kept separate from the
-# camera and used to update the camera basis whenever there is a change.
-# See https://docs.godotengine.org/en/stable/tutorials/3d/using_transforms.html#setting-information.
-var camera_rot_x : float = 0.0
-var camera_rot_y : float = 0.0
-var camera_rot_z : float = 0.0
 
 # Arena
 const arena_scene = preload("res://arena.tscn")
 var arena : Arena = null
-var arena_size := Vector3(5.0, 3.0, 10.0)
+var arena_size := Vector3(5.0, 4.0, 10.0)
 
 func _init() -> void:
 	arena = arena_scene.instantiate()
@@ -25,24 +17,7 @@ func _init() -> void:
 
 func _ready() -> void:
 	camera.position = Vector3(
-		4, 3, 0.95 * max(arena_size.x, arena_size.z))
-
-# Handle mouse inputs to control direction of camera
-func _unhandled_input(event : InputEvent) -> void:
-	if ((event is InputEventMouseButton) and
-		(event.button_index == MOUSE_BUTTON_LEFT) and event.pressed):
-		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		else:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-		get_viewport().set_input_as_handled()
-
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		if event is InputEventMouseMotion:
-			camera_rot_x -= event.relative.x * camera_turn_sensitivity
-			camera_rot_y -= event.relative.y * camera_turn_sensitivity
-			update_camera_rotation()
-			get_viewport().set_input_as_handled()
+		4, 4, 0.95 * max(arena_size.x, arena_size.z))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta : float) -> void:
@@ -51,10 +26,4 @@ func _process(delta : float) -> void:
 		"camera_move_left", "camera_move_right", "camera_move_forward", "camera_move_back")
 	var direction = (camera.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	camera.position += direction * camera_move_speed * delta
-
-# Update the camera location to the current rotation values.
-func update_camera_rotation() -> void:
-	camera.transform.basis = Basis()
-	camera.rotate_object_local(Vector3.UP, camera_rot_x)
-	camera.rotate_object_local(Vector3.RIGHT, camera_rot_y)
-	camera.rotate_object_local(Vector3.FORWARD, camera_rot_z)
+	camera.look_at(arena.position)
