53bc66efd18be610d870274a88a0462a60262ca9
David Goodwin <david@goodwinz.net>
2023-10-25 12:54:42 -0700

    Add first-person camera

--- godot/main.gd
+++ godot/main.gd
@@ -16,14 +16,22 @@ func _init() -> void:
 	add_child(arena)
 
 func _ready() -> void:
+	$CameraPivot/Camera.current = false
 	camera.position = Vector3(
 		4, 4, 0.95 * max(arena_size.x, arena_size.z))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta : float) -> void:
+	# Toggle between first-person and arena cameras.
+	if Input.is_action_just_pressed("toggle_arena_camera"):
+		if $CameraPivot/Camera.current:
+			$CameraPivot/Camera.current = false
+		else:
+			$CameraPivot/Camera.make_current()
+
 	# Move the camera...
 	var input_dir := Input.get_vector(
-		"camera_move_left", "camera_move_right", "camera_move_forward", "camera_move_back")
+		"arena_camera_move_left", "arena_camera_move_right", "arena_camera_move_forward", "arena_camera_move_back")
 	var direction = (camera.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	var new_position : Vector3 = camera.position + direction * camera_move_speed * delta
 	if new_position.length() > 2.0:
