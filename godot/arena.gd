af0fd822403331d981d96e15fcc0fe4e88d099d0
David Goodwin <david@goodwinz.net>
2023-10-25 14:49:49 -0700

    Add arena walls

--- godot/arena.gd
+++ godot/arena.gd
@@ -1,26 +1,26 @@
-class_name Arena extends MeshInstance3D
+class_name Arena extends Node3D
+
+# The overall size of the arena. A const export would be useful here but
+# not supported so use the size() function to get this value.
+const arena_size := Vector3(5.0, 4.0, 10.0)
 
 # Wizards
 const wizard_scene = preload("res://wizard.tscn")
 var wiz : Wizard = null
 var opp : Wizard = null
 
-var initial_arena_size : Vector3
-
-func initialize(n : String, sz : Vector3) -> void:
-	name = n
-	initial_arena_size = sz
+# Get the size of the arena
+func size() -> Vector3:
+	return arena_size
 
 func _ready() -> void:
-	mesh.size = Vector2(initial_arena_size.x, initial_arena_size.z)
-
 	wiz = wizard_scene.instantiate()
-	wiz.initialize("wiz", Vector3(0, 0, initial_arena_size.z / 2.0 - 0.5), Vector3(0, 180.0, 0))
+	wiz.initialize("wiz", Vector3(0, 0, arena_size.z / 2.0 - 0.5), Vector3(0, 180.0, 0))
 	wiz.add_camera()
 
 	add_child(wiz)
 	opp = wizard_scene.instantiate()
-	opp.initialize("opp", Vector3(0, 0, 0.5 - initial_arena_size.z / 2))
+	opp.initialize("opp", Vector3(0, 0, 0.5 - arena_size.z / 2))
 	add_child(opp)
 
 func _process(delta : float) -> void:
@@ -28,4 +28,3 @@ func _process(delta : float) -> void:
 	if Input.is_action_just_pressed("wizard_fireball"):
 		var fireball = wiz.create_fireball()
 		add_child(fireball)
-
--- godot/main.gd
+++ godot/main.gd
@@ -5,20 +5,12 @@ extends Node
 # Meters per second that the camera moves
 var camera_move_speed : int = 20
 
-# Arena
-const arena_scene = preload("res://arena.tscn")
-var arena : Arena = null
-var arena_size := Vector3(5.0, 4.0, 10.0)
-
-func _init() -> void:
-	arena = arena_scene.instantiate()
-	arena.initialize("arena", arena_size)
-	add_child(arena)
+@onready var arena : Arena = $Arena
 
 func _ready() -> void:
 	$CameraPivot/Camera.current = false
 	camera.position = Vector3(
-		4, 4, 0.95 * max(arena_size.x, arena_size.z))
+		4, 4, 0.95 * max(arena.size().x, arena.size().z))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta : float) -> void:
